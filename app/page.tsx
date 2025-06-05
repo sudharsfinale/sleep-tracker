77ae79aa33f7d330f20c6ada6d4a6e712c5ee9bd
sudharsfinale <sudharsfinale@gmail.com>
2025-06-05 23:52:02 +0530

    Add about contact and guest page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
+import Guest from '@/components/Guest';
+import { currentUser } from '@clerk/nextjs/server';
 import React from 'react'
 
-const HomePage = () => {
+const HomePage = async() => {
+  const user = await currentUser();
+  
+    // Check for current logged in clerk user
+  
+    if (!user) {
+      return <Guest />;
+    }
   return (
     <div className='text-red-400'>HomePage</div>
   )
